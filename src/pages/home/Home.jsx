ecfc2c0b112da608057e75fa1f4a54dd2f47762b
Andy Orvis <aorvis@ebuyer.com>
2020-06-18 13:10:15 +0100

    Fixing conflicts with button branch

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Page, Button } from '../../components'
-=======
-import { Page } from '../../components';
->>>>>>> origin/setup
 
 const pageTitle = 'Home';
 
 export const Home = () => (
   <Page pageTitle={pageTitle}>
     <h1>{pageTitle}</h1>
-<<<<<<< HEAD
     <p className="w-1/2 text-center mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, placeat! Ratione quia maiores quas nisi culpa libero, non velit, aperiam maxime sint minus amet consectetur architecto distinctio quod cumque quae!</p>
     <div className="text-center my-8">
       <Button>Button</Button>
@@ -20,12 +15,5 @@ export const Home = () => (
       <Button loading>Link</Button>
       <Button disabled>Link</Button>
     </div>
-=======
-    <p className="w-1/2 text-center mx-auto">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, placeat!
-      Ratione quia maiores quas nisi culpa libero, non velit, aperiam maxime
-      sint minus amet consectetur architecto distinctio quod cumque quae!
-    </p>
->>>>>>> origin/setup
   </Page>
 );
